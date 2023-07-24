e22c5141d3af4ffa5b0a678961a051e9d39823d6
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-07-24 22:48:55 +0200

    Add project files

---
+++ Lazuli/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Lazuli.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Lazuli/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Lazuli.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
