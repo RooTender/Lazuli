b220f369fbaaeab1c44721ccc089f11c49d414ba
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-09-26 00:43:58 +0200

    Setup project (#1)
    * Add project files
    
    * Relocate solution file
    
    * Add sonarcloud analysis
    
    * Fix code smells
    
    * Small update

---
+++ Lazuli/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Lazuli.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public static string Greeting => "Welcome to Lazuli!";
+}
---
+++ Lazuli/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Lazuli.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
